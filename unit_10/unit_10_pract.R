1ffe4d7e8c91c425ceb9735a9a6b72cc52f9d7c4
Chris Byrd <byrds12@outlook.com>
2024-11-25 13:47:25 -0800

    Adds unit 9 practice file and practice data

---
+++ unit_10/unit_10_pract.R
@@ -0,0 +1,26 @@
+library(plyr)
+library(DescTools)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+library(hrbrthemes)
+library(tidyverse)
+library(viridis)
+library(forcats)
+library(readxl)
+library(knitr)
+library(addinslist)
+library(clipr)
+library(gmodels)
+library(Hmisc)
+
+Module_10_PA_Data_1_ <- read_excel("data/Module 10 PA Data(1).xlsx", 
+                                   col_types = c("numeric", "numeric", "numeric", 
+                                                 "numeric"))
+Module_10_PA_Data_1 <- as.data.frame(Module_10_PA_Data_1_)
+
+cor(Module_10_PA_Data_1)
+
+rcorr(as.matrix(Module_10_PA_Data_1), type = "pearson")
+
+rsq <- function (x, y) cor(x, y) ^ 2
