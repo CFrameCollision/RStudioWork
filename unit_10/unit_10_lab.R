aa741d989d5d590ddffa567a86b1dd87d8dd7ee8
Chris Byrd <byrds12@outlook.com>
2024-11-26 19:06:23 -0800

    Further work on unit 10
    Additional work on .Rmd file.

--- unit_10/unit_10_lab.R
+++ unit_10/unit_10_lab.R
@@ -20,3 +20,15 @@ Module_10_Lab_Data <- read_excel("data/Module 10 Lab Data.xlsx",
 
 rcorr(as.matrix(Module_10_Lab_Data), type = "pearson")
 
+rsq <- function (x, y) cor(as.matrix(x), as.matrix(y)) ^ 2
+
+rsq(Module_10_Lab_Data, Module_10_Lab_Data)
+
+graphdata <- as.data.frame(Module_10_Lab_Data)
+
+ggplot(data = graphdata, aes(x = Age,y = `Income Interval`)) +
+  geom_point(shape = 18, color = "darkred") +
+  geom_smooth(method = lm) +
+  xlim(0, 100) +
+  ylim(0, 8) +
+  theme_ipsum()
