d44591b8cccb0cbed0111b1cd2cd30a27567446b
Chris Byrd <byrds12@outlook.com>
2024-12-07 11:52:49 -0800

    Add final unit
    Add final unit and start to final data analysis script

---
+++ final/final_rscript.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(extrafont)
+library(ggplot2)
+library(ggthemes)
+library(hrbrthemes)
+library(thematic)
+library(extrafontdb)
+library(readxl)
+library(colorspace)
+library(addinslist)
+library(clipr)
+library(gmodels)
+library(Hmisc)
+library(RColorBrewer)
+library(DescTools)
+library(viridis)
+
+library(readxl)
+Final_Exam_Data <- read_excel("data/Final Exam Data.xlsx", 
+                              col_types = c("numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric"))
